72f2d96af43b3355ead7763c1261a185334e8486
indiciez <justinmajura@gmail.com>
2022-08-28 22:38:12 +0300

    optimized some configurations

--- docs/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    require_once ('../vendor/autoload.php');
-
-    $Test = new \Indiciez\Tuiz\QueryBuilder();
-    print_r($Test::rawQuery("SELECT * FROM tbl_employee LIMIT 3"));
